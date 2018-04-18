f442ceed2b977a1ae5ed1d0b2a3c822f9c54d24f
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 16:44:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(number)
   number_range = (2..(number-1)).to_a
   check = true
   if number < 2
-    check = false
   else
+    check = false
     number_range.each do |num|
       if number % num == 0
         check false
