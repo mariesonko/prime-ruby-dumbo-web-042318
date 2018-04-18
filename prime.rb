3efbeebeea77c0392bbf25ba57c013e409917ae1
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 16:49:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  number_range = (2..(number-1)).to_a
+  if number < 2
+      return false
+  else
+    number_range.each do |num|
+      if number % num == 0
+        return true
+      end
+    end
+  end
+end
+end 
